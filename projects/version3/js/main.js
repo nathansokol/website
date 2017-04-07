03ab92e86c15ba9e17a7f0c4db0e5a75cb75a95a
njsokol <njsokol@us.ibm.com>
2017-04-07 15:45:52 -0500

    Homepage is finished

--- projects/version3/js/main.js
+++ projects/version3/js/main.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
     //AboutColorChanger
     var colors = ['rgba(26, 188, 156, 0.3)', 'rgba(46, 204, 113, 0.3)', 'rgba(52, 152, 219, 0.3)', 'rgba(155, 89, 182, 0.3)', 'rgba(241, 196, 15, 0.3)', 'rgba(230, 126, 34, 0.3)', 'rgba(231, 76, 60, 0.3)'];
     //Assign Random Colors @ the start
-    $('.aboutReviewTextContainer').each(function() {
+    $('.about-review-text-container').each(function() {
         var color = Math.floor(Math.random() * colors.length);
         $(this).css('background-color', colors[color]);
     });
-    $('.aboutReviewTextContainer').hover(function() {
+    $('.about-review-text-container').hover(function() {
         var color = Math.floor(Math.random() * colors.length);
         $(this).css('background-color', colors[color]);
     });
