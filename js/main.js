a5e399f83597f4dea3183e24e680d8dc2fe04c71
njsokol <njsokol@us.ibm.com>
2017-04-10 16:16:51 -0500

    re-arranged

--- js/main.js
+++ js/main.js
@@ -1,51 +1,29 @@
 $(document).ready(function() {
 
-	$("#workshopMoreExamples").click(function() {
-	    $('#workshopMoreExamples').magnificPopup({
-		    items: [
-		      {
-		        src: 'images/camping2015.png'
-		      },
-		      {
-		        src: 'images/prototype1.png'
-		      },
-		      {
-		        src: 'images/beers.png'
-		      },
-		      {	      
-		        src: 'images/justan1.png'
-		      },
-		      {
-		        src: 'images/metrom1.png'
-		      },
-		      {
-		        src: 'images/sonic.png'
-		      }
-
-		    ],
-		    gallery: {
-		      enabled: true,
-		      preload: [0,3],
-		      navigateByImgClick: true
-		    },
-		    type: 'image' // this is default type
-		});
-	});
-
-	$(".jobTitle").click(function() {
-	  $(this).next('div').toggle('slow');
-	});
-
-	$("#workshopProject1").click(function() {
-	  window.location = "lib/project_dgextreme/project_dgextreme.html";
-	});
-	$("#workshopProject2").click(function() {
-	  window.location = "lib/project_qt/qt_home.html";
-	});
-	$("#workshopProject3").click(function() {
-	  window.location = "http://www.mtpweekend.com";
-	});
-	$("#workshopProject4").click(function() {
-	  window.location = "http://www.mtpweekend.com/halfmarathon";
-	});
+    //AboutColorChanger
+    var colors = ['rgba(26, 188, 156, 0.3)', 'rgba(46, 204, 113, 0.3)', 'rgba(52, 152, 219, 0.3)', 'rgba(155, 89, 182, 0.3)', 'rgba(241, 196, 15, 0.3)', 'rgba(230, 126, 34, 0.3)', 'rgba(231, 76, 60, 0.3)'];
+    //Assign Random Colors @ the start
+    $('.about-review-text-container').each(function() {
+        var color = Math.floor(Math.random() * colors.length);
+        $(this).css('background-color', colors[color]);
+    });
+    $('.about-review-text-container').hover(function() {
+        var color = Math.floor(Math.random() * colors.length);
+        $(this).css('background-color', colors[color]);
+    });
 });
+
+//Scrolling Function for Nav Bar
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('header').addClass('shrink-header');
+    } else {
+        $('header').removeClass('shrink-header');
+    }
+});
+
+// Navigation
+function changePage(target) {
+    $('section.page').removeClass('active');
+    $('#' + target).addClass('active');
+};
